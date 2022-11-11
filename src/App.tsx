ab81df106474db394e7eb937b061ca17dc8e7368
huanghejin <huanghejin@realibox.com>
2022-11-11 17:35:12 +0800

    添加配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { LegacyRef, useRef, useState } from 'react'
+import {Upload} from 'tus-js-client'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [file, setFile] = useState<File | undefined>()
+
+
+  const handleSubmit = () => {
+    if (!file) {
+      return
+    }
+
+    send_request()
+
+
+    // var upload = new Upload(file, {
+    //   endpoint: "http://localhost:8081/upload",
+    //   metadata: {
+    //     filename: file.name,
+    //     filetype: file.type
+    //   },
+    //   onBeforeRequest(req) {
+    //     return new Promise(resolve => {
+    //       var xhr = req.getUnderlyingObject()
+    //       xhr.withCredentials = true
+    //       resolve(xhr)
+    //   })
+    //   },
+    // })
+
+    // upload.start()
+  }
+
+
+  function send_request() {
+    var xmlhttp = null;
+    if (window.XMLHttpRequest) {
+      xmlhttp = new XMLHttpRequest();
+    }
+  
+    if (xmlhttp != null) {
+      // serverUrl是 用户获取 '签名和Policy' 等信息的应用服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+     var serverUrl =
+        "http://localhost:8081/upload";
+  
+      xmlhttp.open("POST", serverUrl, false);
+      const body = new FormData()
+
+      body.append("file", file || '')
+      xmlhttp.send(body)
+      // xmlhttp.onprogress = (num, index) => {
+      //   console.log(num, index);
+      // }
+      return xmlhttp.responseText;
+    } else {
+      alert("Your browser does not support XMLHTTP.");
+    }
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="file" onChange={(e) => setFile(e.target.files?.[0] as File) } />
+
+     <button onClick={handleSubmit} >
+      提交测试
+     </button>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "./",
+  server: {
+    port: 8081,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8081/",
+        // target: 'http://localhost:9200',
+        changeOrigin: true,
+        rewrite: (path: string) => {
+          // console.log('http://ali.testops.top:9199' + path.replace(/^\/api/, ''));
+          return path.replace(/^\/api/, "");
+        },
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@myPages": path.resolve(__dirname, "src/view"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+    },
+  },
+});
